7fed6eee63e6cc662e2c7c0d4ac3b597ae95ec9c
nikensss <rikitzzz@gmail.com>
2020-04-12 13:04:01 +0200

    added Notary class and untracking tests folder

---
+++ classes/Notary.js
@@ -0,0 +1,39 @@
+const fs = require('fs').promises;
+const path = require('path');
+const pdflib = require('pdf-lib');
+
+class Notary {
+  constructor() {}
+
+  async sign(file, signature) {
+    console.log('[Notary] signing ', file);
+    console.log('[Notary] signing with ', signature);
+    try {
+      const pngImageBytes = await fs.readFile(signature);
+
+      const pdfBytes = await fs.readFile(file);
+      const pdf = await pdflib.PDFDocument.load(pdfBytes);
+
+      const pngImage = await pdf.embedPng(pngImageBytes);
+
+      const pngDims = pngImage.scale(0.06);
+
+      const page = pdf.getPages()[1];
+
+      page.drawImage(pngImage, {
+        x: (3 * page.getWidth()) / 7,
+        y: (5.21 * page.getHeight()) / 14,
+        width: pngDims.width,
+        height: pngDims.height,
+      });
+
+      const pdfBytesSaved = await pdf.save();
+      const newVersion = await fs.writeFile(file.replace('.pdf', '_firmat.pdf'), pdfBytesSaved);
+      console.log('[Notary] done signing ', file);
+    } catch (ex) {
+      console.error('Exception recorded', ex);
+    }
+  }
+}
+
+module.exports = Notary;
